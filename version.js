e6cec8d6b572f9e8dca486b71a0d0d564bd5da49
ErickZuniga <12397465+ErickZuniga@users.noreply.github.com>
2024-07-22 17:41:06 -0600

    Styling

---
+++ version.js
@@ -0,0 +1,13 @@
+// version.js
+const currentVersion = "1.18"; // Store the version number here
+
+// Function to update HTML elements with the version number
+function updateVersionInHTML() {
+  const versionElements = document.querySelectorAll(".version-number");
+  versionElements.forEach(element => {
+    element.textContent = currentVersion;
+  });
+}
+
+// Call the function to update the version when the page loads
+window.onload = updateVersionInHTML;
